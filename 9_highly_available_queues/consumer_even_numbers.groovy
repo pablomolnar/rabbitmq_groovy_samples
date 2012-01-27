3dab4290b766b8dc84bbccebdb06bb51819fbe3d
Pablo Molnar <pablomolnar@gmail.com>
2012-01-27 18:33:09 -0300

    Highly available resources

---
+++ 9_highly_available_queues/consumer_even_numbers.groovy
@@ -0,0 +1,28 @@
+import com.rabbitmq.client.*
+
+// Annotate the import is not working :S
+@Grab(group='com.rabbitmq', module='amqp-client', version='2.5.1')
+class Dummy {}
+
+try{
+
+// Connect
+def connectionFactory = new ConnectionFactory([username: 'guest', password: 'guest', virtualHost: '/'])
+def connection = connectionFactory.newConnection(['127.0.0.1', 5672] as Address)
+def channel = connection.createChannel()
+
+// Consume
+QueueingConsumer consumer = new QueueingConsumer(channel)
+channel.basicConsume('evenQueue', true, consumer)
+
+while(true) {
+	QueueingConsumer.Delivery delivery = consumer.nextDelivery()
+	def msg = new String(delivery.body)
+	println msg
+}
+
+// Close
+channel.close()
+connection.close()
+
+} catch(e){e.printStackTrace()}
---
+++ 9_highly_available_queues/producer.groovy
@@ -0,0 +1,39 @@
+import com.rabbitmq.client.*
+
+// Annotate the import is not working :S
+@Grab(group='com.rabbitmq', module='amqp-client', version='2.5.1')
+class Dummy {}
+
+try{
+
+// Connect
+def connectionFactory = new ConnectionFactory([username: 'guest', password: 'guest', virtualHost: '/'])
+def connection = connectionFactory.newConnection(['127.0.0.1', 5672] as Address)
+def channel = connection.createChannel()
+
+// Declare entry poing exchange
+channel.exchangeDeclare('all_numbers', "fanout", true) // Durable exchange
+
+// Route all messages to amq.headers exchange
+channel.exchangeBind('amq.headers', 'all_numbers', '')
+
+//  All numbers durable queue
+channel.queueDeclare('allQueue', true, false, false, ["x-ha-policy": "all"]) // Durable exchange and HA policy
+channel.queueBind('allQueue', 'all_numbers', '')
+
+//  Even numbers durable queue
+channel.queueDeclare('evenQueue', true, false, false, ["x-ha-policy": "all"]) // Durable exchange and HA policy
+channel.queueBind('evenQueue', 'amq.headers', '', ['even': 'true', 'x-match': 'all']) 
+
+100.times {
+  def headers = [even: (it % 2 == 0) as String]
+  def properties = new AMQP.BasicProperties.Builder().headers(headers).deliveryMode(2).build() // Delivery mode 2: persistent
+	channel.basicPublish('all_numbers', '', properties, "Message $it with headers $headers".getBytes())
+  Thread.sleep(100)
+}
+
+// Close
+channel.close()
+connection.close()
+
+} catch(e){e.printStackTrace()}
